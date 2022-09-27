0aefd2bf5219d02677c10a9c64bf0a708a492c00
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2022-09-27 11:24:18 +0530

    plotting validation accuracy comparison

--- rare_features_spurious_correlations.py
+++ rare_features_spurious_correlations.py
@@ -9,12 +9,12 @@ import numpy as np
 train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype("float32") / 255
 
-train_images_with_noise_channels = np.concenate(
+train_images_with_noise_channels = np.concatenate(
     [train_images, np.random.random((len(train_images), 784))],
     axis=1
 )
 
-train_images_with_zeros_channels = np.concenate(
+train_images_with_zeros_channels = np.concatenate(
     [train_images, np.zeros((len(train_images), 784))],
     axis=1
 )
@@ -48,3 +48,17 @@ history_zeros = model.fit(
     batch_size=128,
     validation_split=0.2
 )
+
+# plotting validation accuracy comparison
+import matplotlib.pyplot as plt
+val_acc_noise = history_noise.history["val_accuracy"]
+val_acc_zeros = history_zeros.history["val_accuracy"]
+epochs = range(1, 11)
+plt.plot(epochs, val_acc_noise, "b-",
+         label="Validation accuracy with noise channels")
+plt.plot(epochs, val_acc_zeros, "b--",
+         label="Validation accuracy with zeros channels")
+plt.title("Effect of noise channels on validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
