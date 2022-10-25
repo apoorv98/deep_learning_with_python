3e9c99b12bdcab185463a5f793c2bd8443d697b4
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2022-10-25 13:27:44 +0530

    training convnet on MNIST images

--- convnet.py
+++ convnet.py
@@ -12,3 +12,19 @@ x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
 x = layers.Flatten()(x)
 outputs = layers.Dense(10, activation="softmax")(x)
 model = keras.Model(inputs=inputs, outputs=outputs)
+
+# training the convnet on MNIST images
+from tensorflow.keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype("float32") / 255
+
+model.compile(optimizer="rmsprop",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+model.fit(train_images, train_labels,
+          epochs=5,
+          batch_size=64)
