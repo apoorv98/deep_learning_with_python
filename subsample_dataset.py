47934b6d2915062dccb267edf903c741a70a0d81
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2022-10-27 09:56:48 +0530

    preprocess using image_dataset_from_directory

--- subsample_dataset.py
+++ subsample_dataset.py
@@ -47,3 +47,23 @@ model = keras.Model(inputs=inputs, outputs=outputs)
 model.compile(loss="binary_crossentropy",
               optimizer="rmsprop",
               metrics=["accuracy"])
+
+
+# data preprocessing
+from tensorflow.keras.utils import image_dataset_from_directory
+
+train_dataset = image_dataset_from_directory(
+    new_base_dir / "train",
+    image_size=(180, 180),
+    batch_size=32
+)
+validation_dataset = image_dataset_from_directory(
+    new_base_dir / "validation",
+    image_size=(180, 180),
+    batch_size=32
+)
+test_dataset = image_dataset_from_directory(
+    new_base_dir / "test",
+    image_size=(180, 180),
+    batch_size=32
+)
