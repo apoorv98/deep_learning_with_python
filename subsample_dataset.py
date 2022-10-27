f69f4f1c64e51f229d97969674d350b9561dce1d
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2022-10-27 09:50:57 +0530

    convnet model instance

--- subsample_dataset.py
+++ subsample_dataset.py
@@ -21,3 +21,24 @@ def make_subset(subset_name, start_index, end_index):
 make_subset("train", start_index=0, end_index=1000)
 make_subset("validation", start_index=1000, end_index=1500)
 make_subset("test", start_index=1500, end_index=2500)
+
+
+# instantiating small convnet for dogs vs cats
+from tensorflow import keras
+from tensorflow.keras import layers
+
+inputs = keras.Input(shape=(180, 180, 3))
+x = layers.Rescaling(1./255)(inputs)
+x = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=64, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=128, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.MaxPooling2D(pool_size=2)(x)
+x = layers.Conv2D(filters=256, kernel_size=3, activation="relu")(x)
+x = layers.Flatten()(x)
+outputs = layers.Dense(1, activation="sigmoid")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
